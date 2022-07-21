29d69c3e0f2ac2c3590e42f287977e67041adff9
SilasAggrey <91203655+SilasAggrey@users.noreply.github.com>
2022-07-21 12:10:40 +0800

    New

--- src/index.js
+++ src/index.js
@@ -1,35 +1,92 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+
+
 const server = express();
+server.use(bodyParser.json());
 
-const handleAllTypesOfRequests = (req,res) => {
-  res.send("Response from server.use")
+let booksDb = [
+
+];
+
+class BooksModel {
+  constructor({title, author, description}){
+    this.title = title;
+    this.author = author;
+    this.description = description;
+  }
+
+   save() {
+    booksDb.push(this);
+    return this;
+   }
+
+   static all() {
+    return booksDb;
+   }
+
+   static update(updateInfo = {}){ 
+
+    booksDb = booksDb.map(book => {
+        if(book.title === updateInfo.title) {
+            return {...book, ...updateInfo};
+        }
+    return booksDb;
+   });
+   }
+   static delete([title]) {
+        let deleteBook = null;
+         booksDb.filter(book => {
+           if(book.title !== title){
+              
+                return true;
+           }
+            deleteBook = book;
+              return false;
+   });
+    return deleteBook;
 }
 
-const handleProfilePutRequestType =(req,res)=> {
-    res.send("Responded to profile request with method of value put");
 }
 
-const handleLoginDeleteRequestType =(req,res)=> {
-    res.send("Responded to login request with method of value delete");
+
+const listBooks = (req, res) => {
+  const books = BooksModel.all();
+  res.json({data: books});
 }
 
-const handleProfileDeleteRequestType =(req,res)=> {
-    res.send("A responds to profile request with method of value delete");
+const createBook = (req, res) => {
+
+    const {title, author, description} = req.body;
+    const book = new BooksModel({title, author, description});
+    book.save();
+    res.json({message: "Book created", data: book});
 }
 
-server.post("/profile", handleAllTypesOfRequests);
-server.put("/profile", handleProfilePutRequestType);
-server.get("/profile", (req,res) => res.send("Hello, this is a profile page using get method"));
-server.patch("/profile", (req,res) => res.send("Hello, this is a profile page using patch method"));
-server.delete("/profile", handleProfileDeleteRequestType);
+const updateBook = (req, res) => {
 
-server.get("/login", (req,res) => res.send("Hello, this is a login page"));
-server.delete("/login", handleLoginDeleteRequestType);
-server.put("/login", (req,res) => res.send("Hello, this is a login page using put method" ));
-server.patch("/login", (req,res) => res.send("Hello, this is a login page using patch method"));
-server.post("/login", (req,res) => res.send("Hello, this is a login page using the patch method"));
+    const {title, author, description} = req.body;
+    const updatedBook = BooksModel.update({title, author, description});
+    res.json({message: "Book updated", data: updatedBook});
 
-server.patch("/signup", (req,res) => res.send("Hello, this is a login page with method value patch"));
+}
+
+const deleteBook = (req, res) => {  
+    
+        const {title} = req.body;
+        const deletedBook = BooksModel({title});
+        res.json({message: "Book deleted", data: deletedBook});
+}    
+
+server.get("/book", listBooks); 
+
+server.post("/book", createBook)
+
+server.put("/book", updateBook)
+
+server.delete("/book", deleteBook)
 
 
-server.listen(3000, () => console.log("server is ready"));
+server.listen(3000, () => 
+  console.log("server running on port 3000"));
